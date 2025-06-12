d085fc6f182d5b7239b702de65211ca30553eb9b
Alisson Oliveira <alisson_sao@hotmail.com>
2025-06-12 13:20:42 -0300

    change file main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import tensorflow as tf
+import epi_detector
+
+if __name__ == '__main__':
+    detector = epi_detector.EPIDetector(
+        dataset_dir='dataset',
+        img_size=128,
+        batch_size=16,
+        epochs=10
+    )
+    
+    # Se ainda não treinou o modelo:
+    detector.preprocess_data()
+    detector.build_model()
+    detector.train()
+    detector.save_model()
+
+    # Ou, se já tiver treinado o modelo e quiser apenas executar:
+    # detector.load_model()
+
+    detector.run_realtime_detection()
